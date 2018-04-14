c3909ad465f6fd0d807bc4d624c5c4564dc3b1ed
sunlggggg <sunlggggg@gmail.com>
2018-04-14 22:57:15 +0800

    modify

--- Serve.js
+++ Serve.js
@@ -1,5 +1,12 @@
 var http = require('http');
 
+function getClientIp(req) {
+    return req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
+}
+
 http.createServer(function (request, response) {
 
     // 发送 HTTP 头部 
@@ -8,8 +15,8 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
 
     // 发送响应数据 "Hello World"
-    response.end('Hello World\n');
+    response.end(getClientIp(request));
 }).listen(8888);
 
 // 终端打印如下信息
-console.log('Server running at http://127.0.0.1:8888/');
+console.log('Server running at http://111.230.101.228:8888/');
